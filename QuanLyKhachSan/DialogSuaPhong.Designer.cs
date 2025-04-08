ee9c5bda9ba35cc3319a0f67efeb970b4d50b937
DEV00000001 <vuongdachaivang@gmail.com>
2025-04-08 07:52:35 +0700

    check

---
+++ QuanLyKhachSan/DialogSuaPhong.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace QuanLyKhachSan
+{
+    partial class DialogSuaPhong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btncancel = new System.Windows.Forms.Button();
+            this.btnsua = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cbbtinhtrang = new System.Windows.Forms.ComboBox();
+            this.cbbtang = new System.Windows.Forms.ComboBox();
+            this.cbbloaiphong = new System.Windows.Forms.ComboBox();
+            this.nummax = new System.Windows.Forms.NumericUpDown();
+            this.txttrangbi = new System.Windows.Forms.TextBox();
+            this.txtgia = new System.Windows.Forms.TextBox();
+            this.txtdientich = new System.Windows.Forms.TextBox();
+            this.txtsophong = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nummax)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btncancel
+            // 
+            this.btncancel.BackColor = System.Drawing.Color.Red;
+            this.btncancel.Cursor = System.Windows.Forms.Cursors.No;
+            this.btncancel.Location = new System.Drawing.Point(622, 376);
+            this.btncancel.Name = "btncancel";
+            this.btncancel.Size = new System.Drawing.Size(75, 23);
+            this.btncancel.TabIndex = 5;
+            this.btncancel.Text = "Hủy";
+            this.btncancel.UseVisualStyleBackColor = false;
+            // 
+            // btnsua
+            // 
+            this.btnsua.BackColor = System.Drawing.Color.Yellow;
+            this.btnsua.Cursor = System.Windows.Forms.Cursors.No;
+            this.btnsua.Location = new System.Drawing.Point(495, 376);
+            this.btnsua.Name = "btnsua";
+            this.btnsua.Size = new System.Drawing.Size(75, 23);
+            this.btnsua.TabIndex = 4;
+            this.btnsua.Text = "Sửa";
+            this.btnsua.UseVisualStyleBackColor = false;
+            this.btnsua.Click += new System.EventHandler(this.btnsua_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbbtinhtrang);
+            this.groupBox1.Controls.Add(this.cbbtang);
+            this.groupBox1.Controls.Add(this.cbbloaiphong);
+            this.groupBox1.Controls.Add(this.nummax);
+            this.groupBox1.Controls.Add(this.txttrangbi);
+            this.groupBox1.Controls.Add(this.txtgia);
+            this.groupBox1.Controls.Add(this.txtdientich);
+            this.groupBox1.Controls.Add(this.txtsophong);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Cursor = System.Windows.Forms.Cursors.No;
+            this.groupBox1.Location = new System.Drawing.Point(31, 51);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(738, 289);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sửa Phòng";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // cbbtinhtrang
+            // 
+            this.cbbtinhtrang.FormattingEnabled = true;
+            this.cbbtinhtrang.Location = new System.Drawing.Point(463, 33);
+            this.cbbtinhtrang.Name = "cbbtinhtrang";
+            this.cbbtinhtrang.Size = new System.Drawing.Size(165, 24);
+            this.cbbtinhtrang.TabIndex = 16;
+            // 
+            // cbbtang
+            // 
+            this.cbbtang.FormattingEnabled = true;
+            this.cbbtang.Location = new System.Drawing.Point(141, 189);
+            this.cbbtang.Name = "cbbtang";
+            this.cbbtang.Size = new System.Drawing.Size(164, 24);
+            this.cbbtang.TabIndex = 15;
+            // 
+            // cbbloaiphong
+            // 
+            this.cbbloaiphong.FormattingEnabled = true;
+            this.cbbloaiphong.Location = new System.Drawing.Point(141, 87);
+            this.cbbloaiphong.Name = "cbbloaiphong";
+            this.cbbloaiphong.Size = new System.Drawing.Size(164, 24);
+            this.cbbloaiphong.TabIndex = 14;
+            // 
+            // nummax
+            // 
+            this.nummax.Location = new System.Drawing.Point(464, 142);
+            this.nummax.Name = "nummax";
+            this.nummax.Size = new System.Drawing.Size(163, 22);
+            this.nummax.TabIndex = 13;
+            // 
+            // txttrangbi
+            // 
+            this.txttrangbi.Location = new System.Drawing.Point(463, 192);
+            this.txttrangbi.Name = "txttrangbi";
+            this.txttrangbi.Size = new System.Drawing.Size(164, 22);
+            this.txttrangbi.TabIndex = 12;
+            // 
+            // txtgia
+            // 
+            this.txtgia.Location = new System.Drawing.Point(141, 142);
+            this.txtgia.Name = "txtgia";
+            this.txtgia.Size = new System.Drawing.Size(164, 22);
+            this.txtgia.TabIndex = 10;
+            // 
+            // txtdientich
+            // 
+            this.txtdientich.Location = new System.Drawing.Point(464, 84);
+            this.txtdientich.Name = "txtdientich";
+            this.txtdientich.Size = new System.Drawing.Size(164, 22);
+            this.txtdientich.TabIndex = 9;
+            // 
+            // txtsophong
+            // 
+            this.txtsophong.Location = new System.Drawing.Point(141, 36);
+            this.txtsophong.Name = "txtsophong";
+            this.txtsophong.Size = new System.Drawing.Size(164, 22);
+            this.txtsophong.TabIndex = 8;
+            this.txtsophong.TextChanged += new System.EventHandler(this.txtsophong_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(351, 192);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 16);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Trang Bị";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(351, 87);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(58, 16);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Diện tích";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(351, 142);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(106, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Số Người Tối Đa";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(351, 36);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Tình Trạng";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 192);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Tầng";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Giá";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Loại Phòng";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Số Phòng";
+            // 
+            // DialogSuaPhong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btncancel);
+            this.Controls.Add(this.btnsua);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "DialogSuaPhong";
+            this.Text = "DialogSuaPhong";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nummax)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btncancel;
+        private System.Windows.Forms.Button btnsua;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cbbtinhtrang;
+        private System.Windows.Forms.ComboBox cbbtang;
+        private System.Windows.Forms.ComboBox cbbloaiphong;
+        private System.Windows.Forms.NumericUpDown nummax;
+        private System.Windows.Forms.TextBox txttrangbi;
+        private System.Windows.Forms.TextBox txtgia;
+        private System.Windows.Forms.TextBox txtdientich;
+        private System.Windows.Forms.TextBox txtsophong;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
